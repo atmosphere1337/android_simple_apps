63c18f86ef4d70e7af8315ecb05e52d3bf8c46fa
atmosphere1337 <agbdlcider@gmail.com>
2023-12-07 00:41:40 +0700

    calc sketch. ui setup and simple button->output link

---
+++ app/src/main/java/com/example/android_simple_apps/Calc.kt
@@ -0,0 +1,55 @@
+package com.example.android_simple_apps
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class Calc : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view : View = inflater.inflate(R.layout.fragment_calc, container, false)
+        var output = view.findViewById<TextView>(R.id.calc_output)
+        view.findViewById<Button>(R.id.btn_go_calc_menu).setOnClickListener {
+            parentFragmentManager.beginTransaction().replace(R.id.mainframe, Menu()).commit()
+        }
+        view.findViewById<Button>(R.id.calc_btn0).setOnClickListener { char_input("0", output) }
+        view.findViewById<Button>(R.id.calc_btn1).setOnClickListener { char_input("1", output) }
+        view.findViewById<Button>(R.id.calc_btn2).setOnClickListener { char_input("2", output) }
+        view.findViewById<Button>(R.id.calc_btn3).setOnClickListener { char_input("3", output) }
+        view.findViewById<Button>(R.id.calc_btn4).setOnClickListener { char_input("4", output) }
+        view.findViewById<Button>(R.id.calc_btn5).setOnClickListener { char_input("5", output) }
+        view.findViewById<Button>(R.id.calc_btn6).setOnClickListener { char_input("6", output) }
+        view.findViewById<Button>(R.id.calc_btn7).setOnClickListener { char_input("7", output) }
+        view.findViewById<Button>(R.id.calc_btn8).setOnClickListener { char_input("8", output) }
+        view.findViewById<Button>(R.id.calc_btn9).setOnClickListener { char_input("9", output) }
+
+        view.findViewById<Button>(R.id.calc_btnmul).setOnClickListener { char_input("*", output) }
+        view.findViewById<Button>(R.id.calc_btndiv).setOnClickListener { char_input("/", output) }
+        view.findViewById<Button>(R.id.calc_btnsub).setOnClickListener { char_input("-", output) }
+        view.findViewById<Button>(R.id.calc_btnadd).setOnClickListener { char_input("+", output) }
+        view.findViewById<Button>(R.id.calc_btndot).setOnClickListener { char_input(".", output) }
+
+        view.findViewById<Button>(R.id.calc_btndel).setOnClickListener { char_del(output) }
+        view.findViewById<Button>(R.id.calc_btnac).setOnClickListener { output_clear(output) }
+        return view
+    }
+    public fun char_input(digit : String, view :  TextView) {
+        view.text = view.text.toString() + digit;
+    }
+    public fun char_del(view: TextView) {
+        if (view.text.isNotEmpty())
+            view.text = view.text.toString().substring(0, view.text.length - 1)
+    }
+    public fun output_clear(view : TextView) {
+        view.text = ""
+    }
+}
